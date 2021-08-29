c1aa71548e5c1415b7a4d5d0d0e35a09903bc96e
Justin Jondle <jondle@gmail.com>
2021-08-29 12:57:02 -0700

    Added PowerShell help and simple README for GitHub

--- Compare-FileHash.ps1
+++ Compare-FileHash.ps1
@@ -1,3 +1,54 @@
+<#
+.SYNOPSIS
+Checks a file against a hash checksum to confirm it is complete and hasn't been altered.
+
+.DESCRIPTION
+Checks a file against a hash checksum to confirm it is complete and hasn't been altered.
+It can input either a static hash to test against or a file containing the expected hash
+and filename with each file on its own line in the format:
+<hash> *<filename>
+
+.PARAMETER Path
+Path and filename of the file to test the hash for.
+
+.PARAMETER TestHash
+A static hash to test the Path file against.
+
+.PARAMETER TestHashFile
+The path and filename of a hash file. The hash file should contain the hash for multiple files with each file on a separate line in the format:
+<hash> *<filename>
+
+.PARAMETER HashAlgorithm
+OPTIONAL The hash algorthim to check. Valid options are SHA512, SHA256, SHA1, and MD5. If not provided, SHA256 will be used.
+
+.PARAMETER Verbose
+OPTIONAL If provided, all output will be a string for console output and will include the hashes.
+
+.INPUTS
+None. You cannot pipe objects to Compare-FileHash.
+
+.OUTPUTS
+System.Boolean. True if the calculated hash matches the supplied hash, otherwise False.
+
+If -Verbose is provided all output will be System.String, is intended for console output, and will include the hashes.
+
+.EXAMPLE
+PS> Compare-FileHash.ps1 -Path 'C:\temp\ubuntu-21.04-desktop-amd64.iso' -TestHash 'fa95fb748b34d470a7cfa5e3c1c8fa1163e2dc340cd5a60f7ece9dc963ecdf88'
+True
+
+.EXAMPLE
+PS> Compare-FileHash.ps1 -Path 'C:\temp\ubuntu-21.04-desktop-amd64.iso' -TestHashFile 'C:\temp\SHA256SUMS.txt' -Verbose
+Hash                                                             InputFrom
+----                                                             ---------
+fa95fb748b34d470a7cfa5e3c1c8fa1163e2dc340cd5a60f7ece9dc963ecdf88 InputFile
+FA95FB748B34D470A7CFA5E3C1C8FA1163E2DC340CD5A60F7ECE9DC963ECDF88 Calculated From File
+
+
+Matched: True
+
+.LINK
+http://www.fabrikam.com/extension.html
+#>
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory = $true, ParameterSetName = 'InputHash')]
---
+++ README.md
@@ -0,0 +1,7 @@
+#Description
+Checks a file against a hash checksum to confirm it is complete and hasn't been altered.
+It can input either a static hash to test against or a file containing the expected hash
+and filename with each file on its own line in the format:
+\<hash\> \*\<filename\>
+
+See PowerShell help for more details.
